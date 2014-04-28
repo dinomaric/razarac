5395f8a9857c64edbbeb99fb121a87105f121638
Dino Maric <dino.maric@wizard.ba>
2014-04-28 11:30:28 +0200

    Dodavanje klase drzave

---
+++ WebApplication1/WebApplication1/Drzave.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class Drzave
+    {
+        public string CountryCode { get; set; }
+
+        public string CountryName { get; set; }
+
+    }
+}
